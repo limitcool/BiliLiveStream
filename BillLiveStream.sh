1caaba60cd7d5f8d9f61b7fba5c3eb7903f77afb
limitcool <initcoool@gmail.com>
2021-09-05 17:38:47 +0800

    change room_id

--- BillLiveStream.sh
+++ BillLiveStream.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 # 读取配置文件
+DATE=$(date +%y-%m-%d-%H:%M:%S)
 source conf
 # 检测直播源
 if [[ $1 =~ twitch ]]; then
    echo "twitch直播"
    twitch=$1
-   M3U8=$(youtube-dl -g ${twitch} --no-check-certificate --proxy=${proxy}) ||  echo "Twitch直播未开始"
+   M3U8=$(youtube-dl -g ${twitch} --no-check-certificate --proxy=${proxy}) || echo "Twitch直播未开始"
    echo $M3U8
    if [ -n "$M3U8" ]; then
       :
@@ -25,13 +26,22 @@ fi
 
 # 调用接口进行开播
 echo "开始直播"
-start=$("curl --location --request POST 'https://api.live.bilibili.com/room/v1/Room/startLive' --header 'cookie: ${cookie}' --header 'Content-Type: application/x-www-form-urlencoded' --data-urlencode 'room_id={$roomid}' --data-urlencode 'platform=pc' --data-urlencode 'area_v2=${area}'--data-urlencode 'csrf_token=${csrf_token}' --data-urlencode 'csrf=${csrf_token}'")
-echo  "开播接口返回" $start >> /var/log/ffmpeg.log
+startlive=$(
+   curl --location --request POST 'https://api.live.bilibili.com/room/v1/Room/startLive' \
+      --header "cookie: ${cookie}" \
+      --header 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36' \
+      --header 'Content-Type: application/x-www-form-urlencoded' \
+      --data-urlencode "room_id=${room_id}" \
+      --data-urlencode 'platform=pc' \
+      --data-urlencode "area_v2=${area}" \
+      --data-urlencode "csrf_token=${csrf_token}" \
+      --data-urlencode "csrf=${csrf_token}"
+)
+echo "$DATE 开播接口返回" $startlive >>/var/log/ffmpeg.log
 
 while :; do               #loop循环，为了让模块一直运行
    sleep 5                # 每次检测时间5秒
    ffmpeg=$(pgrep ffmpeg) #检查ffmpeg是否在运行
-   DATE=$(date +%y-%m-%d-%H:%M:%S)
    if [ -n "$ffmpeg" ]; then
       echo $DATE "ffmpeg正在运行" >>/var/log/ffmpeg.log
    #正确输入信息到日志文件
